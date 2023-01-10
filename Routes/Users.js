2e424159ae1a8cc9141d44dd3f2502b76d4e2e6e
Sebastian Ionel <70603934+DalmatianuSebikk@users.noreply.github.com>
2023-01-10 21:24:24 +0200

    Added Users CRUD operations

--- Routes/Users.js
+++ Routes/Users.js
@@ -7,4 +7,64 @@ router.get("/", async (req, res) => {
   const users = await User.findAll();
   res.send(users);
 });
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  res.send(user);
+})
+
+router.get("/name/:name", async (req, res) => {
+  const user = await User.findOne({where: { user_name: req.params.name}});
+  res.send(user);
+})
+
+router.post("/create", async (req, res) => {
+  User.create({ 
+    user_name: req.body.user_name, 
+    email: req.body.email,
+    password: req.body.password,
+    display_name: req.body.display_name,
+    address: req.body.address,
+    postal_code: req.body.postal_code
+  }).then(() => {
+      res.send("User has been created!");
+  }).catch((err) => {
+      res.send("Eroare: Introduceti datele corect!");
+  });
+})
+
+router.put("/update/:id", async(req,res) => {
+  User.update(
+  {
+    user_name: req.body.user_name, 
+    email: req.body.email,
+    password: req.body.password,
+    display_name: req.body.display_name,
+    address: req.body.address,
+    postal_code: req.body.postal_code
+  }, 
+  {
+    where: {
+      user_id: req.params.id
+    }
+  }
+   ).then(() => {
+    res.send("User has been updated yuhuuuuu")
+  }).catch( (err) => {
+    res.send("Eroare, verifica daca tot ce ai modificat e ocei");
+  })
+})
+
+
+router.delete("/delete/:id", async(req, res) => {
+  User.destroy({
+    where: {
+      user_id: req.params.id
+    }
+  }).then( () => {
+    res.send("Userul a fost sters");
+  }).catch( (err) => {
+    res.send("Eroare ");
+  })
+})
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { sequelize } = require("./models");
 const { DataTypes } = require("sequelize");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const usersRoute = require("./Routes/Users.js");
 
+app.use("/", express.json());
 app.use("/users", usersRoute);
 
 sequelize
